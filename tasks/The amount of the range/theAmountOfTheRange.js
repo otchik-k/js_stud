0599141ba23fc8803fd85a2a8a73c5e29a1fcd68
KIRILL OTCHIK <67601079+otchik-k@users.noreply.github.com>
2022-02-17 22:52:15 +0300

    Create theAmountOfTheRange.js

---
+++ tasks/The amount of the range/theAmountOfTheRange.js
@@ -0,0 +1,57 @@
+// Функция, которая принимает начальное и конечное значение и возвращает массив, содержащий все числа между ними.
+// A function that takes a start and end value and returns an array containing all the numbers between them.
+function range (start, end, step = 1) {
+  let numbers = [];
+  let count = 0;
+  // Проверка, являются ли значения числовыми?
+  // Checking whether the values are numeric?
+  if (typeof start === 'number' && typeof end === 'number') {
+    if (start < end){
+      while (start <= end){
+        numbers[count] = start;
+        count++;
+        start = start + step;
+      }
+      return numbers;
+    }
+      else {
+        while (start >= end) {
+          numbers[count] = start;
+          count++;
+          start = start - step;
+        }
+        return numbers;
+      }
+    }
+  else {
+    // Сообщение, если переменные не числовые.
+    // Message if the variables are not numeric.
+    console.log ('Переданные знаяения не являются числами');
+  }
+}
+
+// Функция выводитзначения элементов массива.
+// The function outputs the values of the array elements.
+function showArray (array) {
+  for (let element of array) {
+    console.log(element);
+  }
+}
+
+// Функция возвращает ыумму всех элементов массива.
+// // The function returns the sum of all the elements of the array.
+function sum (array) {
+  let count = 0;
+  let sumArray = 0;
+  if (typeof array === 'object'){
+    for (element of array){
+      sumArray = sumArray + element;
+    }
+    return sumArray;
+  }
+  else {
+    console.log('Переданное значение не является массивом')
+  }
+}
+
+console.log(sum (range (1, 10)));
